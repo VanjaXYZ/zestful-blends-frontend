2e351330f89768d6ece0db624ceeaaf6d0475365
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-10-10 08:29:46 +0100

    code: refactor

--- src/app/custom_components/header/TopNavigation.tsx
+++ src/app/custom_components/header/TopNavigation.tsx
@@ -1,24 +1,11 @@
-import LogoImage from "./LogoImage";
-import NavIcons from "./NavIcons";
-import NavLinkItems from "./NavLinkItems";
-import NavOrderButton from "./NavOrderButton";
-import styles from "./TopNavigation.module.css";
+import DesktopNavigationMenu from "./DesktopNavigationMenu";
+import MobileNavigationMenu from "./MobileNavigationMenu";
 
 const TopNavigation = () => {
   return (
-    <header
-      className={`flex h-52 bg-primary-green justify-between flex-col sm:flex-row py-4 px-12 w-full ${styles.bottomWave}`}
-    >
-      <div className="flex sm:w-1/2 gap-6">
-        <LogoImage />
-        <NavLinkItems />
-      </div>
-      <div className="sm:w-1/2">
-        <div className="flex gap-6 justify-end items-center">
-          <NavIcons />
-          <NavOrderButton />
-        </div>
-      </div>
+    <header className="w-full">
+      <DesktopNavigationMenu />
+      <MobileNavigationMenu />
     </header>
   );
 };
