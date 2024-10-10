eec396350e8848d44d9a74b4c9d026d974132d10
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-10-10 08:58:08 +0100

    created

---
+++ src/app/custom_components/header/mobile-design/MobileNavigationMenu.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { IoMenu } from "react-icons/io5";
+
+const MobileNavigationMenu = () => {
+  return (
+    <nav className="flex justify-end">
+      <Sheet>
+        <SheetTrigger>
+          <IoMenu size={42} color="white" />
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </nav>
+  );
+};
+
+export default MobileNavigationMenu;
