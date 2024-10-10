5270cab9714c2e872aa76a8e2d0226349a20f10a
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-10-10 08:57:51 +0100

    style: add dynamic bg color

--- src/app/custom_components/header/NavOrderButton.tsx
+++ src/app/custom_components/header/NavOrderButton.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
 import React from "react";
 
-const NavOrderButton = () => {
+const NavOrderButton = ({ textColor }: { textColor: string }) => {
   return (
-    <Button className="bg-primary-yellow text-black font-bold hover:bg-primary-yellow-dark-shade">
+    <Button
+      className={`bg-primary-yellow ${textColor} font-bold hover:bg-primary-yellow-dark-shade`}
+    >
       Order Now
     </Button>
   );
