43de39f0e460e4f60f355cfa7cc0d34369587a38
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-10-13 01:36:19 +0100

    style: padding fix

--- src/app/custom_components/header_info/HeaderInfo.tsx
+++ src/app/custom_components/header_info/HeaderInfo.tsx
@@ -5,7 +5,7 @@ import InfoHeaderEmail from "./InfoHeaderEmail";
 
 const HeaderInfo = () => {
   return (
-    <div className="justify-between bg-primary-yellow-dark-shade py-2 w-full flex-col sm:flex-row hidden lg:flex">
+    <div className="justify-between bg-primary-yellow-dark-shade py-2 px-12 w-full flex-col sm:flex-row hidden lg:flex">
       <InfoHeaderIcons />
       <InfoHeaderMessage />
       <InfoHeaderEmail />
