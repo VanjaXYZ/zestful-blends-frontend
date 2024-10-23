faf097e034c713f47f32ac5d1ce85902dc3d626b
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-10-23 18:11:56 +0100

    ui: add breadrcumbs

--- src/app/(pages)/blogs/page.tsx
+++ src/app/(pages)/blogs/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import BlogList from "./components/BlogList";
+import Breadcrumbs from "@/app/custom_components/shared/Breadcrumbs";
 
 const page = () => {
   return (
     <main className="bg-primary-olive">
-      <BlogList />
+      <Breadcrumbs currentPageName="Blogs" />
+      <section className="lg:mt-24">
+        <BlogList />
+      </section>
     </main>
   );
 };
