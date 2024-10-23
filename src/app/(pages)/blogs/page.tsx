4d42d60dc500c628e43c436d777d221f3b6e4bfa
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-10-23 18:05:46 +0100

    created

---
+++ src/app/(pages)/blogs/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BlogList from "./components/BlogList";
+
+const page = () => {
+  return (
+    <main className="bg-primary-olive">
+      <BlogList />
+    </main>
+  );
+};
+
+export default page;
