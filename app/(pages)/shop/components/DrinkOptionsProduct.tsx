38c11e22b5aadfeca94358754bc8480d0dac9472
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2025-01-11 08:26:55 +0100

    add new ts interface & pass whole drink object as prop

--- app/(pages)/shop/components/DrinkOptionsProduct.tsx
+++ app/(pages)/shop/components/DrinkOptionsProduct.tsx
@@ -1,24 +1,25 @@
-import Image from 'next/image';
-import React from 'react'
+import Image from "next/image";
 
-const DrinkOptionsProduct = ({ productName, imageSrc } : {
-    productName: string;
-    imageSrc: string
-}) => {
-  return (
-    <div className='flex justify-center flex-col items-center'>
-      <Image
-        src={imageSrc}
-        width={300}
-        height={300}
-        alt='Product Image'
-        className='rounded-full'
-      />
-      <h2 className='pt-12 font-bold'>
-        {productName}
-      </h2>
-    </div>
-  )
+interface NewProductVisualProps {
+  id: string;
+  imageSrc: string;
+  productName: string;
+  isNewProduct: boolean;
 }
 
-export default DrinkOptionsProduct
+const DrinkOptionsProduct = ({ drink }: { drink: NewProductVisualProps }) => {
+  return (
+    <div className="flex justify-center flex-col items-center">
+      <Image
+        src={drink.imageSrc}
+        width={300}
+        height={300}
+        alt="Product Image"
+        className="rounded-full"
+      />
+      <h2 className="pt-12 font-bold">{drink.productName}</h2>
+    </div>
+  );
+};
+
+export default DrinkOptionsProduct;
