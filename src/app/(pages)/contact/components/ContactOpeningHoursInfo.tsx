4196229674fdf9438f300140824a589680c77404
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2025-01-11 22:15:44 +0100

    add text size and weight

--- src/app/(pages)/contact/components/ContactOpeningHoursInfo.tsx
+++ src/app/(pages)/contact/components/ContactOpeningHoursInfo.tsx
@@ -3,12 +3,12 @@ import React from "react";
 const ContactOpeningHoursInfo = () => {
   return (
     <section>
-      <h2 className="text-6xl text-white font-bold ">Opening Hours</h2>
+      <h2 className="text-6xl text-white font-bold">Opening Hours</h2>
       <ul className="pt-6 text-white space-y-4">
-        <li className="list-disc">
+        <li className="list-disc text-xl font-semibold">
           Monday To Friday ............. 08.00 - 18.00
         </li>
-        <li className="list-disc">
+        <li className="list-disc text-xl font-semibold">
           Saturday & Sunday ............ 08.00 - 18.00
         </li>
       </ul>
