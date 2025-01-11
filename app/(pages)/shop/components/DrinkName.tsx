cf3ec336d26391cf25a8df168467d94af4dd6b56
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2025-01-11 08:26:10 +0100

    removed div

--- app/(pages)/shop/components/DrinkName.tsx
+++ app/(pages)/shop/components/DrinkName.tsx
@@ -1,21 +1,16 @@
-import { Button } from '@/components/ui/button';
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
 
-const DrinkName = ({
-    productName
-}: {
-    productName: string;
-}) => {
+const DrinkName = ({ productName }: { productName: string }) => {
   return (
-    <div className='flex justify-center'>
-      <Button className='bg-primary-dark-orange font-semibold rounded-full 
-      w-[150px] hover:bg-primary-orange transition-colors mb-6 text-black
-      flex'>
-        {productName}
-      </Button>
-    </div>
-  )
-}
+    <Button
+      className="bg-primary-dark-orange font-semibold rounded-full 
+      w-[150px] hover:bg-primary-orange transition-colors mb-6 text-white
+      flex"
+    >
+      {productName}
+    </Button>
+  );
+};
 
-export default DrinkName
-
+export default DrinkName;
