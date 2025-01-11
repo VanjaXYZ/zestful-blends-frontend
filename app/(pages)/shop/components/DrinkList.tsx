cd72f2d847ce3d1b12a7682fa8968218b386cddc
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2025-01-11 08:25:54 +0100

    add margin-bottom

--- app/(pages)/shop/components/DrinkList.tsx
+++ app/(pages)/shop/components/DrinkList.tsx
@@ -1,45 +1,42 @@
-import React from 'react'
-import DrinkName from './DrinkName'
+import React from "react";
+import DrinkName from "./DrinkName";
 
 const items = [
-    {
-        id: "1",
-        productName: "Fresh Juice"
-    },
-    {
-        id: "2",
-        productName: "Ice Cream Juice"
-    },
-    {
-        id: "3",
-        productName: "Healthy Drinks"
-    },
-    {
-        id: "4",
-        productName: "Party Drinks"
-    },
-    {
-        id: "5",
-        productName: "Milkshakes"
-    }
-]
+  {
+    id: "1",
+    productName: "Fresh Juice",
+  },
+  {
+    id: "2",
+    productName: "Ice Cream Juice",
+  },
+  {
+    id: "3",
+    productName: "Healthy Drinks",
+  },
+  {
+    id: "4",
+    productName: "Party Drinks",
+  },
+  {
+    id: "5",
+    productName: "Milkshakes",
+  },
+];
 
 const DrinkList = () => {
   return (
-    <section className='bg-[#fdd79d] py-8 flex flex-col'>
-        <h2 className='text-2xl text-center lg:text-4xl text-crimson-red font-bold py-16'>
-            Items Collection
-        </h2>
-        <div className='grid md:grid-flow-col-dense'>
-            {items?.map((item: any) => (
-                <DrinkName 
-                    productName={item.productName}
-                    key={item.id}
-                />
-            ))}
-        </div>
+    <section className="bg-[#fdd79d] py-8 flex flex-col">
+      <h2 className="text-2xl text-center lg:text-4xl text-crimson-red font-bold py-16">
+        Items Collection
+      </h2>
+      <div className="flex flex-wrap justify-center gap-8 mt-8">
+        {items?.map((item: any) => (
+          <DrinkName productName={item.productName} key={item.id} />
+        ))}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default DrinkList
+export default DrinkList;
