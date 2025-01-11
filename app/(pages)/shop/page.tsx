778d529eb6124474b3762a4748d014ea9195b1e9
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2025-01-11 08:25:42 +0100

    add margin bottom

--- app/(pages)/shop/page.tsx
+++ app/(pages)/shop/page.tsx
@@ -1,16 +1,16 @@
-import Breadcrumbs from '@/app/custom_components/shared/Breadcrumbs'
-import React from 'react'
-import ShopPage from './components/ShopPage'
+import Breadcrumbs from "@/app/custom_components/shared/Breadcrumbs";
+import React from "react";
+import ShopPage from "./components/ShopPage";
 
 const page = () => {
   return (
-    <main className=''>
-        <Breadcrumbs currentPageName='Shop' />
-        <section className="lg:mt-24">
-            <ShopPage />
-        </section>
+    <main className="">
+      <Breadcrumbs currentPageName="Shop" />
+      <section className="lg:mt-24 mb-8">
+        <ShopPage />
+      </section>
     </main>
-  )
-}
+  );
+};
 
-export default page
+export default page;
