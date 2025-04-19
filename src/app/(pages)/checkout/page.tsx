bfcb7b439a5662b0c58fe0c063cbc7200b955273
Dawid <dawid.oleksy94@gmail.com>
2025-04-19 05:46:39 +0200

    bottomWave, one div->section

--- src/app/(pages)/checkout/page.tsx
+++ src/app/(pages)/checkout/page.tsx
@@ -11,22 +11,25 @@ const page = () => {
 
   return (
     <main>
-      <section className={`absolute bg-[#ED7905] w-full justify-center top-[145px] h-36 max-lg:h-16 z-[100] flex flex-col items-center gap-4`}>
+      <div className={`absolute bg-[#ED7905] w-full justify-center top-[145px] h-36 max-lg:h-16 z-[100] flex flex-col items-center gap-4`}>
         <h2 className="text-4xl text-white font-bold">Check Out!</h2>
-      </section>
+      </div>
       {/* <Breadcrumbs currentPageName="Check Out!" bgColor="bg-[#ED7905]"/> */}
       <div className={`w-full absolute h-32 max-sm:h-32  ${styles.waveOrange} z-150 bg-[#ED7905]`}></div>
-      <div className={`flex max-sm:flex-col max-sm:px-8 px-[5%] pt-8 pb-32 bg-[#FBEEAC] ${styles.bottomWave}></div> `}>
-        <div className='w-[55%] max-sm:w-full flex flex-col items-start'>
-          <div className='w-[80%] max-sm:w-full'>
-            <h1 className="text-[#ED7905] font-bold text-5xl pt-32 max-sm:pt-4 pb-16">Ready To Refresh Yourself?</h1>
+      <div className='flex flex-col'>
+        <section className={`flex max-sm:flex-col max-sm:px-8 px-[5%] pt-8 pb-28 max-lg:pb-16 max-sm:pb-4 bg-[#FBEEAC] ${styles.bottomWave}></div> `}>
+          <div className='w-[55%] max-sm:w-full flex flex-col items-start'>
+            <div className='w-[80%] max-sm:w-full'>
+              <h1 className="text-[#ED7905] font-bold text-5xl pt-32 max-sm:pt-4 pb-16">Ready To Refresh Yourself?</h1>
+            </div>
+            <ContactInfo/>
+            <PaymentMethod/>
           </div>
-          <ContactInfo/>
-          <PaymentMethod/>
-        </div>
-        <div className='w-[45%] max-sm:w-full flex justify-center'>
-          <MyOrderReview/>
-        </div>
+          <div className={`w-[45%] max-sm:w-full flex justify-center`}>
+            <MyOrderReview/>
+          </div>
+        </section>
+        <div className={`w-full h-16 bg-[#FBEEAC] z-150 ${styles.bottomWave}`}></div>
       </div>
     </main>
   )
