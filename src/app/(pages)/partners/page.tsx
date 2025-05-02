590d40f925e190b267ae21bde8597093e8bf5183
aitheenti <navneelkuppili@gmail.com>
2025-05-02 15:01:56 -0400

    faq section and update

--- src/app/(pages)/partners/page.tsx
+++ src/app/(pages)/partners/page.tsx
@@ -6,10 +6,55 @@ import LycheeDream from "@/assets/Lush_Lychee_Dream.png";
 import LushLychee from "@/assets/lush-lychee-dream.png";
 import PearPlum from "@/assets/pear-plum.png";
 import PearPlumPizza from "@/assets/pear-plum-pizzazz.png";
+import Serve from "@/assets/serve.png";
 import ImageCell from "./components/ImageCell";
 import { Button } from "@/components/ui/button";
 import InfoAndImage from "./components/InfoAndImage";
+import AccordionElements from "../faq/components/AccordionElements";
 
+export type AccordionProps = {
+  id: string;
+  header: string;
+  text: string;
+};
+
+const faqAccordionData: AccordionProps[] = [
+  {
+    id: "1",
+    header: "What's your busiest delivery time window?",
+    text: "Our juices are made from 100% natural ingredients—fruits, vegetables, and superfoods, with no added sugars or preservatives. We prepare each juice fresh to order to ensure maximum freshness and nutrient retention.",
+  },
+  {
+    id: "2",
+    header: "What's your minimum order volume requirement for partnerships?",
+    text: "We package our juices in eco-friendly, recyclable bottles that maintain freshness. When stored in the refrigerator, our cold-pressed juices can last up to 3 days. For the best taste and quality, we recommend consuming them as soon as possible.",
+  },
+  {
+    id: "3",
+    header: "Why don't you offer in-house delivery?",
+    text: "We focus on crafting juices while trusting experts like you for logistics.",
+  },
+  {
+    id: "4",
+    header: " Are your juices really fresh if delivered?",
+    text: "Yes! We offer monthly subscriptions with a discount. Fresh juices are delivered weekly to your door according to your chosen plan, so you can always have healthy, delicious juices on hand.",
+  },
+  {
+    id: "5",
+    header: "How do you handle customer complaints about delivery?",
+    text: 'Our recommended juice for detox is the "Green Detox" made with kale, spinach, cucumber, lemon, and ginger. It helps cleanse the body, boosts immunity, and provides natural energy for the entire day.',
+  },
+  {
+    id: "6",
+    header: "How quickly do you prepare orders after receiving alerts",
+    text: 'Our recommended juice for detox is the "Green Detox" made with kale, spinach, cucumber, lemon, and ginger. It helps cleanse the body, boosts immunity, and provides natural energy for the entire day.',
+  },
+  {
+    id: "7",
+    header: "How do I know which delivery service covers my area?",
+    text: 'Our recommended juice for detox is the "Green Detox" made with kale, spinach, cucumber, lemon, and ginger. It helps cleanse the body, boosts immunity, and provides natural energy for the entire day.',
+  },
+];
 const page = () => {
   return (
     <main>
@@ -42,7 +87,7 @@ const page = () => {
           <ImageCell sourceImage={LycheeDream} altText="Lush lychee dream" />
         </div>
       </section>
-      <section className="flex flex-col p-5 bg-[#FBEEAC63]">
+      <section className="flex flex-col p-5 bg-[#FBEEAC63] shadow-[0_0_10px_#f8a100]">
         <div className="flex justify-center text-4xl text-[#2D4E75] font-bold m-5">
           <h2>Click the link to deliver</h2>
         </div>
@@ -95,6 +140,18 @@ const page = () => {
             "No temperature disputes (shelf-stable for 2hr delivery window)",
           ]}
         />
+      </section>
+
+      <section className="flex justify-around p-10 bg-[#FBEEAC63]">
+        <div className="flex flex-col w-1/2 align-center justify-around text-4xl text-[#2D4E75] font-bold m-5">
+          <div>Frequently Asked Questions</div>
+          <div>
+            <AccordionElements elements={faqAccordionData} />
+          </div>
+        </div>
+        <div className="flex w-1/3 text-[#2D4E75] justify-around">
+          <ImageCell sourceImage={Serve} altText="Lush lychee dream" />
+        </div>
       </section>
     </main>
   );
