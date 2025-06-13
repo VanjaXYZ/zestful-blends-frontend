a162b72c88f4f18375716df0ef39d98b2b93c78f
aitheenti <navneelkuppili@gmail.com>
2025-06-13 11:18:16 -0400

    Cart and wishlist popover setup

--- src/app/custom_components/header/NavIcons.tsx
+++ src/app/custom_components/header/NavIcons.tsx
@@ -1,20 +1,116 @@
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@radix-ui/react-popover";
 import { FaShoppingBag, FaUser } from "react-icons/fa";
 import { MdFavorite } from "react-icons/md";
+import CartItem from "./CartItem";
+import { cartItems } from "@/lib/mock/CartItems";
 
 const NavIcons = () => {
   let hasProductsInCart = false;
-  const productsInCart = ["Apple juice", "Strawberry slim shake"];
+  const productsInCart = cartItems;
   if (productsInCart.length > 0) hasProductsInCart = true;
   return (
     <div className="flex gap-8 max-xl:gap-4 relative">
       <FaUser color="white" size={26} className="cursor-pointer" />
-      <MdFavorite color="white" size={26} className="cursor-pointer" />
-      <FaShoppingBag color="white" size={26} className="cursor-pointer" />
-      {hasProductsInCart && (
-        <span className="absolute -top-2 -right-3 font-bold bg-primary-yellow-dark-shade rounded-full w-5 h-5 flex items-center justify-center">
-          {productsInCart.length}
-        </span>
-      )}
+      <Popover>
+        <PopoverTrigger asChild>
+          <div className="relative">
+            <MdFavorite color="white" size={26} className="cursor-pointer" />
+            {hasProductsInCart && (
+              <span className="absolute -top-2 -right-2 font-bold bg-primary-yellow-dark-shade rounded-full w-5 h-5 flex items-center justify-center text-xs">
+                {productsInCart.length}
+              </span>
+            )}
+          </div>
+        </PopoverTrigger>
+        <PopoverContent className="w-96 max-h-[60vh] bg-white p-4 rounded-md shadow-lg overflow-y-auto z-[101]">
+          <div className="flex flex-col gap-2 h-full p-2">
+            <div className="flex items-start justify-between">
+              <h2 className="text-primary-yellow font-bold text-3xl pb-4">
+                Wishlist
+              </h2>
+              <p className="flex items-center text-primary-yellow rounded-sm p-2 border border-primary-yellow text-sm">
+                {productsInCart.length} items
+              </p>
+            </div>
+            {productsInCart.map((item) => {
+              return (
+                <>
+                  <CartItem
+                    variant="wishlist"
+                    onQuantityChange={() => {}}
+                    onRemoveFromCart={() => {}}
+                    onEditClick={() => {}}
+                    key={item.name}
+                    name={item.name}
+                    toppings={item.toppings}
+                    imageSrc={item.imageSrc}
+                    price={item.price}
+                    quantity={item.quantity}
+                  />
+                  <hr className="border-t-2 border-primary-yellow w-full my-2" />
+                </>
+              );
+            })}
+          </div>
+        </PopoverContent>
+      </Popover>
+      <Popover>
+        <PopoverTrigger asChild>
+          <div className="relative">
+            <FaShoppingBag color="white" size={26} className="cursor-pointer" />
+            {hasProductsInCart && (
+              <span className="absolute -top-2 -right-2 font-bold bg-primary-yellow-dark-shade rounded-full w-5 h-5 flex items-center justify-center text-xs">
+                {productsInCart.length}
+              </span>
+            )}
+          </div>
+        </PopoverTrigger>
+        <PopoverContent className="w-96 max-h-[60vh] bg-white p-4 rounded-md shadow-lg overflow-y-auto z-[101]">
+          <div className="flex flex-col gap-2 h-full p-2">
+            <div className="flex items-start justify-between">
+              <h2 className="text-primary-yellow font-bold text-3xl pb-4">
+                Your Cart
+              </h2>
+              <p className="text-primary-yellow rounded-sm p-2 border border-primary-yellow text-sm flex items-center">
+                {productsInCart.length} items
+              </p>
+            </div>
+            {productsInCart.map((item) => {
+              return (
+                <>
+                  <CartItem
+                    variant="cart"
+                    onQuantityChange={() => {}}
+                    onRemoveFromCart={() => {}}
+                    onEditClick={() => {}}
+                    key={item.name}
+                    name={item.name}
+                    toppings={item.toppings}
+                    imageSrc={item.imageSrc}
+                    price={item.price}
+                    quantity={item.quantity}
+                  />
+                  <hr className="border-t-2 border-primary-yellow w-full my-2" />
+                </>
+              );
+            })}
+            <div className="flex flex-col gap-2 mt-2 w-full">
+              <div>
+                You choose : <p className="text-primary-yellow inline">ASAP</p>
+              </div>
+              <div className="flex justify-center ">
+                <button className="bg-primary-yellow text-black py-1 px-3 text-sm rounded hover:bg-yellow-500">
+                  Checkout
+                </button>
+              </div>
+            </div>
+          </div>
+        </PopoverContent>
+      </Popover>
     </div>
   );
 };
